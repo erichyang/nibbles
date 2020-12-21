f60dcf5afdc2b02d2ceb9b7695294de214acc193
Eric Yang <erichyang11@gmail.com>
2020-12-21 09:36:56 -0600

    choose command

---
+++ cogs/choose.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+import random
+
+
+class Choose(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    # events
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('Choose online')
+
+    #commands
+    @commands.command()
+    async def choose(self, ctx, *, param):
+        choice = random.choice(param.split(', '))
+        await ctx.send(choice)
+
+
+def setup(client):
+    client.add_cog(Choose(client))
