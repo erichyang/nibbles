004f27ef8805a0c03f5f98f7db98b18202f2bebf
Eric Yang <erichyang11@gmail.com>
2021-07-03 20:36:03 -0500

    delete logging

--- cogs/anime.py
+++ cogs/anime.py
@@ -105,9 +105,6 @@ class Anime(commands.Cog):
         self.interactions = []
 
     async def anime_char_spawn(self, channel):
-        log = await self.client.fetch_channel(819271204468031508)
-        await log.send(f'starting timer for {channel.guild.name}')
-        await asyncio.sleep(random.randint(0, 900))
         chatters = []
         async for message in channel.history(limit=100):
             if message.author.id not in chatters:
@@ -160,7 +157,6 @@ class Anime(commands.Cog):
 
         msg = await channel.send(content=f'Anime character appearance!\n{unarr(pings)}', embed=embed, delete_after=1800)
         await msg.add_reaction('🍪')
-        await log.send(f'character sent to {channel.name} in {channel.guild.name}')
 
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction, user):
